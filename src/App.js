2b1b45470d4ba6ba6d7049a7d41a8acafca9c473
Baswaraj <baswaraj1706@gmail.com>
2024-01-22 05:15:58 +0000

    show/hide clock using Mounting(constructor,render,componentDidMount) & updating state & componenetWillUnmount(in this timeIntervals,API calling, ect...)

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import {Component} from 'react'
+import Clock from './components/Clock'
+
+import './App.css'
+
+class App extends Component {
+  state = {showClock: true}
+
+  clockToggleButton = () => {
+    this.setState(prevState => {
+      const {showClock} = prevState
+      return {
+        showClock: !showClock,
+      }
+    })
+  }
+
+  render() {
+    const {showClock} = this.state
+
+    return (
+      <div className="app-container">
+        <button
+          type="button"
+          className="toggle-button"
+          onClick={this.clockToggleButton}
+        >
+          {showClock ? 'Hide Clock' : 'Show Clock'}
+        </button>
+        {showClock && <Clock />}
+      </div>
+    )
+  }
+}
+
+export default App
